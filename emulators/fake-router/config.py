04428c3cdf90e137bb8e77fb09149b006b50d3ce
vvslohith <vvslohith@gmail.com>
2025-03-02 00:59:22 +0530

    nothing

--- emulators/fake-router/config.py
+++ emulators/fake-router/config.py
@@ -5,5 +5,5 @@ load_dotenv()  # Load environment variables from .env file
 
 MONGO_URI = os.getenv("MONGO_URI")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "fake_router")
-LOG_FILE = "honeypot.log"
+# LOG_FILE = "honeypot.log"
 FLASK_DEBUG = True
--- emulators/fake-router/logger.py
+++ emulators/fake-router/logger.py
@@ -1,11 +1,13 @@
 import logging
-from config import LOG_FILE
+#from config import LOG_FILE
 
-logging.basicConfig(
-    filename=LOG_FILE,
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
-)
+# logging.basicConfig(
+#     filename=LOG_FILE,
+#     level=logging.INFO,
+#     format="%(asctime)s - %(levelname)s - %(message)s"
+# )
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
 def log_event(event):
     logging.info(event)
